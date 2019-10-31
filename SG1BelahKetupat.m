b2471eaa235ef8b3d266fd29f2b5cd00e031205e
Hakimaruuu <57005303+Hakimaruuu@users.noreply.github.com>
2019-10-31 20:37:19 +0700

    Create SG1BelahKetupat.m

---
+++ SG1BelahKetupat.m
@@ -0,0 +1,8 @@
+disp("Kali ini mau buat belah ketupat");
+x = input('Masukkan sisi alas : ');
+y = input('Masukkan tinggi : ');
+luasbelah = x * y;
+kelilingbelah = 2*x + 2*y ;
+disp("Luas belah ketupat = " + luasbelah);
+disp("Keliling belah ketupat = " + kelilingbelah);
+disp(" ");
