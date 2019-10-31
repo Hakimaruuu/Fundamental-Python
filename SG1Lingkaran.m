ba48eab73c288aa98620efb96961acd6e7633fe1
Hakimaruuu <57005303+Hakimaruuu@users.noreply.github.com>
2019-10-31 20:36:02 +0700

    Create SG1Lingkaran.m

---
+++ SG1Lingkaran.m
@@ -0,0 +1,8 @@
+disp("Kali ini mau buat lingkaran");
+x = input('Masukkan jari-jari : ');
+pi = 3.14;
+luasling       = 2 * pi * x * x;
+kelilingling   = 2 * pi + x;
+disp("Luas lingkaran = " + luasling);
+disp("Keliling lingkaran = " + kelilingling);
+disp(" ");
