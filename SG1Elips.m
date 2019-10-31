d61d0832d1c6749d30b9b8393cd2c501c7c8d77c
Hakimaruuu <57005303+Hakimaruuu@users.noreply.github.com>
2019-10-31 20:35:31 +0700

    Create SG1Elips.m

---
+++ SG1Elips.m
@@ -0,0 +1,9 @@
+disp("Kali ini mau buat elips");
+x = input('Masukkan AC elips : ');
+y = input('Masukkan BD elips : ');
+pi = 3.14;
+luaselips       = 0.5 * pi * y * x;
+kelilingelips   = 0.5 * pi + x + y;
+disp("Luas elips = " + luaselips);
+disp("Keliling elips = " + kelilingelips);
+disp(" ");
